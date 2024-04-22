00d6592e22cca81953ea9f8bc729fb1ee38c0bc0
michael-hhai <138688447+michael-hhai@users.noreply.github.com>
2024-04-22 16:48:25 -0400

    Add set_metric function to tracer (#12)

--- src/sdk/tracer.ts
+++ src/sdk/tracer.ts
@@ -1,242 +1,313 @@
-import { v4 as uuidv4 } from 'uuid';
-import axios, { AxiosInstance } from 'axios';
+import { v4 as uuidv4 } from "uuid";
+import axios, { AxiosInstance } from "axios";
 
 export type EventType = "chain" | "generic" | "tool" | "model";
 
 export interface Config {
-    type: string;
-    name?: string;
-    description?: string;
+  type: string;
+  name?: string;
+  description?: string;
 }
 
 export interface ModelConfig extends Config {
-    provider: string;
-    endpoint?: string;
-    model: string;
-    prompt_template?: string;
-    chat_template?: ChatMessage[];
-    temperature?: number;
-    max_tokens?: number;
-    top_p?: number;
-    stop?: string | string[];
-    presence_penalty?: number;
-    frequency_penalty?: number;
-    other?: { [key: string]: any };
+  provider: string;
+  endpoint?: string;
+  model: string;
+  prompt_template?: string;
+  chat_template?: ChatMessage[];
+  temperature?: number;
+  max_tokens?: number;
+  top_p?: number;
+  stop?: string | string[];
+  presence_penalty?: number;
+  frequency_penalty?: number;
+  other?: { [key: string]: any };
 }
 
 export interface ToolConfig extends Config {
-    source?: string;
-    other?: { [key: string]: any };
+  source?: string;
+  other?: { [key: string]: any };
 }
 
 export interface AgentOther {
-    max_iterations: number;
-    stop?: string | string[];
-    output_parser?: string;
+  max_iterations: number;
+  stop?: string | string[];
+  output_parser?: string;
 }
 
 export interface AgentConfig extends Config {
-    agent_class: string;
-    tools: ToolConfig[];
-    model_config: ModelConfig;
-    other: AgentOther;
+  agent_class: string;
+  tools: ToolConfig[];
+  model_config: ModelConfig;
+  other: AgentOther;
 }
 
 export enum ChatRole {
-    User = "user",
-    Assistant = "assistant",
-    System = "system"
+  User = "user",
+  Assistant = "assistant",
+  System = "system",
 }
 
 export interface ChatMessage {
-    role: ChatRole;
-    content: string;
-    name?: string;
+  role: ChatRole;
+  content: string;
+  name?: string;
 }
 
 export interface ParentLog {
-    project?: string;
-    event_id?: string;
-    session_id?: string;
-    event_type?: EventType;
-    event_name?: string;
-    config?: Config | ModelConfig | ToolConfig | AgentConfig;
-    inputs?: { [key: string]: any };
-    outputs?: { [key: string]: any };
-    children?: ChildLog[];
-    user_properties?: { [key: string]: any };
-    metadata?: { [key: string]: any };
-    source?: string;
-    start_time?: number;
-    end_time?: number;
-    duration?: number;
-    error?: string;
-    metrics?: { [key: string]: number };
-    feedback?: { [key: string]: any };
+  project?: string;
+  event_id?: string;
+  session_id?: string;
+  event_type?: EventType;
+  event_name?: string;
+  config?: Config | ModelConfig | ToolConfig | AgentConfig;
+  inputs?: { [key: string]: any };
+  outputs?: { [key: string]: any };
+  children?: ChildLog[];
+  user_properties?: { [key: string]: any };
+  metadata?: { [key: string]: any };
+  source?: string;
+  start_time?: number;
+  end_time?: number;
+  duration?: number;
+  error?: string;
+  metrics?: { [key: string]: number };
+  feedback?: { [key: string]: any };
 }
 
 export interface ChildLog {
-    project?: string;
-    event_id?: string;
-    session_id?: string;
-    parent_id?: string;
-    event_type: EventType;
-    event_name: string;
-    config: Config | ModelConfig | ToolConfig | AgentConfig;
-    inputs: { [key: string]: any };
-    outputs?: { [key: string]: any };
-    children?: ChildLog[];
-    user_properties?: { [key: string]: any };
-    metadata?: { [key: string]: any };
-    source?: string;
-    start_time?: number;
-    end_time?: number;
-    duration?: number;
-    error?: string;
-    metrics?: { [key: string]: number };
-    feedback?: { [key: string]: any };
+  project?: string;
+  event_id?: string;
+  session_id?: string;
+  parent_id?: string;
+  event_type: EventType;
+  event_name: string;
+  config: Config | ModelConfig | ToolConfig | AgentConfig;
+  inputs: { [key: string]: any };
+  outputs?: { [key: string]: any };
+  children?: ChildLog[];
+  user_properties?: { [key: string]: any };
+  metadata?: { [key: string]: any };
+  source?: string;
+  start_time?: number;
+  end_time?: number;
+  duration?: number;
+  error?: string;
+  metrics?: { [key: string]: number };
+  feedback?: { [key: string]: any };
 }
 
 export class SessionTracer {
-    private eventStack: ChildLog[];
-    private client: AxiosInstance;
-    private session_id: string;
-    private parentEvent: ParentLog;
+  private eventStack: ChildLog[];
+  private client: AxiosInstance;
+  private session_id: string;
+  private parentEvent: ParentLog;
+  private lastEventId: string;
+  private lastEventMetrics: { [key: string]: any };
+  private lastEventMetadata: { [key: string]: any };
 
-    constructor(private api_key: string, private project: string, private session_name: string, private user_properties: { [key: string]: any } = {}, private source: string = 'HoneyHive Typescript SDK') {
-        this.eventStack = [];
-        this.client = axios.create({
-          headers: {
-            Authorization: "Bearer " + api_key,
+  constructor(
+    private api_key: string,
+    private project: string,
+    private session_name: string,
+    private user_properties: { [key: string]: any } = {},
+    private source: string = "HoneyHive Typescript SDK",
+  ) {
+    this.eventStack = [];
+    this.client = axios.create({
+      headers: {
+        Authorization: "Bearer " + api_key,
+      },
+    });
+    this.session_id = "";
+    this.parentEvent = {};
+    this.lastEventId = "";
+    this.lastEventMetrics = {};
+    this.lastEventMetadata = {};
+  }
+
+  public getSessionId(): string {
+    return this.session_id;
+  }
+
+  public async setMetric(
+    metric_name: string,
+    metric_value: any,
+    threshold: any,
+  ): Promise<void> {
+    if (!this.lastEventId) {
+      throw new Error("No events defined on session to set metric on");
+    }
+    const metrics: Record<string, any> = { ...this.lastEventMetrics };
+    const metadata: Record<string, any> = { ...this.lastEventMetadata };
+    metrics[metric_name] = metric_value;
+    metadata[`threshold_${metric_name}`] = threshold;
+    const body = {
+      event_id: this.lastEventId,
+      metadata: metadata,
+      metrics: metrics,
+    };
+    try {
+      const res = await this.client.put(
+        `https://api.honeyhive.ai/events`,
+        body,
+      );
+      if (res.status === 200) {
+        this.lastEventMetrics = metrics;
+        this.lastEventMetadata = metadata;
+      }
+    } catch (error) {
+      // continue regardless of error
+    }
+  }
+
+  async startSession(inputs?: { [key: string]: any }): Promise<void> {
+    try {
+      this.session_id = uuidv4();
+      const session = {
+        project: this.project,
+        source: this.source,
+        session_name: this.session_name,
+        user_properties: this.user_properties,
+        inputs: inputs || {},
+        event_id: this.session_id,
+        session_id: this.session_id,
+      };
+      this.parentEvent = {
+        project: this.project,
+        event_type: "chain",
+        event_name: this.session_name,
+        config: { type: "chain" },
+        inputs: inputs || {},
+        start_time: Date.now(),
+        user_properties: this.user_properties,
+        metadata: {},
+        source: this.source,
+        children: [],
+        metrics: {},
+        feedback: {},
+        event_id: uuidv4(),
+        session_id: this.session_id,
+      };
+      await this.client.post(`https://api.honeyhive.ai/session/start`, {
+        session: session,
+      });
+    } catch {
+      // continue regardless of error
+    }
+  }
+
+  startEvent(
+    event_type: EventType,
+    event_name: string,
+    config: Config | ModelConfig | ToolConfig | AgentConfig,
+    inputs: { [key: string]: any },
+  ): void {
+    try {
+      const newEvent: ChildLog = {
+        session_id: this.session_id,
+        event_id: uuidv4(),
+        project: this.project,
+        parent_id:
+          this.eventStack.length > 0
+            ? this.eventStack[this.eventStack.length - 1].event_id
+            : this.parentEvent.event_id,
+        event_type,
+        event_name,
+        config,
+        inputs,
+        start_time: Date.now(),
+        user_properties: this.user_properties,
+        metadata: {},
+        source: this.source,
+        children: [],
+        error: "",
+        metrics: {},
+        feedback: {},
+      };
+      this.eventStack.push(newEvent);
+    } catch {
+      // continue regardless of error
+    }
+  }
+
+  endEvent(outputs?: { [key: string]: any }, error?: string): void {
+    try {
+      if (this.eventStack.length < 1) {
+        throw new Error("No event to end. Stack length must be at least 1.");
+      }
+      const currentEvent = this.eventStack.pop();
+      if (currentEvent) {
+        if (currentEvent.event_id) {
+          this.lastEventId = currentEvent.event_id;
+          if (currentEvent.metrics) {
+            this.lastEventMetrics = currentEvent.metrics;
+          } else {
+            this.lastEventMetrics = {};
+          }
+          if (currentEvent.metadata) {
+            this.lastEventMetadata = currentEvent.metadata;
+          } else {
+            this.lastEventMetadata = {};
+          }
+        }
+        currentEvent.end_time = Date.now();
+        currentEvent.duration =
+          currentEvent.end_time -
+          (currentEvent.start_time || currentEvent.end_time);
+        currentEvent.outputs = outputs;
+        if (error) {
+          currentEvent.error = error;
+        }
+        if (this.eventStack.length > 0) {
+          this.eventStack[this.eventStack.length - 1].children = [
+            ...(this.eventStack[this.eventStack.length - 1].children || []),
+            currentEvent,
+          ];
+        } else {
+          this.parentEvent.children = [
+            ...(this.parentEvent.children || []),
+            currentEvent,
+          ];
+        }
+      }
+    } catch {
+      // continue regardless of error
+    }
+  }
+
+  async endSession(
+    outputs?: { [key: string]: any },
+    error?: string,
+  ): Promise<void> {
+    try {
+      while (this.eventStack.length > 0) {
+        this.endEvent();
+      }
+      const session_trace = this.parentEvent;
+      session_trace.end_time = Date.now();
+      session_trace.duration =
+        session_trace.end_time -
+        (session_trace.start_time || session_trace.end_time);
+      session_trace.outputs = outputs;
+      if (error) {
+        session_trace.error = error;
+      }
+      if (session_trace) {
+        await this.client.post(
+          `https://api.honeyhive.ai/session/${this.session_id}/traces`,
+          {
+            logs: [session_trace],
           },
+        );
+
+        await this.client.put(`https://api.honeyhive.ai/events`, {
+          event_id: this.session_id,
+          outputs: outputs,
         });
-        this.session_id = "";
-        this.parentEvent = {};
+      }
+    } catch {
+      // continue regardless of error
     }
-
-    public getSessionId(): string {
-        return this.session_id;
-    }
-
-    async startSession(inputs?: { [key: string]: any }): Promise<void> {
-        try {
-            this.session_id = uuidv4();
-            const session = {
-                project: this.project,
-                source: this.source,
-                session_name: this.session_name,
-                user_properties: this.user_properties,
-                inputs: inputs || {},
-                event_id: this.session_id,
-                session_id: this.session_id,
-            };
-            this.parentEvent = {
-                project: this.project,
-                event_type: "chain",
-                event_name: this.session_name,
-                config: { type: "chain" },
-                inputs: inputs || {},
-                start_time: Date.now(),
-                user_properties: this.user_properties,
-                metadata: {},
-                source: this.source,
-                children: [],
-                metrics: {},
-                feedback: {},
-                event_id: uuidv4(),
-                session_id: this.session_id,
-            };
-            await this.client.post(
-              `https://api.honeyhive.ai/session/start`,
-              {
-                session: session,
-              },
-            );
-        } catch {
-            // continue regardless of error
-        }
-    }
-
-    startEvent(event_type: EventType, event_name: string, config: Config | ModelConfig | ToolConfig | AgentConfig, inputs: { [key: string]: any }): void {
-        try {
-            const newEvent: ChildLog = {
-                session_id: this.session_id,
-                event_id: uuidv4(),
-                project: this.project,
-                parent_id: this.eventStack.length > 0 ? this.eventStack[this.eventStack.length - 1].event_id : this.parentEvent.event_id,
-                event_type,
-                event_name,
-                config,
-                inputs,
-                start_time: Date.now(),
-                user_properties: this.user_properties,
-                metadata: {},
-                source: this.source,
-                children: [],
-                error: "",
-                metrics: {},
-                feedback: {},
-            };
-            this.eventStack.push(newEvent);
-        } catch {
-            // continue regardless of error
-        }
-    }
-
-    endEvent(outputs?: { [key: string]: any }, error?: string): void {
-        try {
-            if (this.eventStack.length < 1) {
-                throw new Error("No event to end. Stack length must be at least 1.");
-            }
-            const currentEvent = this.eventStack.pop();
-            if (currentEvent) {
-                currentEvent.end_time = Date.now();
-                currentEvent.duration = currentEvent.end_time - (currentEvent.start_time || currentEvent.end_time);
-                currentEvent.outputs = outputs;
-                if (error) {
-                    currentEvent.error = error;
-                }
-                if (this.eventStack.length > 0) {
-                  this.eventStack[this.eventStack.length - 1].children = [...(this.eventStack[this.eventStack.length - 1].children || []), currentEvent];
-                } else {
-                  this.parentEvent.children = [...(this.parentEvent.children || []), currentEvent];
-                }
-            }
-        } catch {
-            // continue regardless of error
-        }
-    }
-
-    async endSession(outputs?: { [key:string]: any }, error?: string): Promise<void> {
-        try {
-            while (this.eventStack.length > 0) {
-                this.endEvent();
-            }
-            const session_trace = this.parentEvent;
-            session_trace.end_time = Date.now();
-            session_trace.duration = session_trace.end_time - (session_trace.start_time || session_trace.end_time);
-            session_trace.outputs = outputs;
-            if (error) {
-                session_trace.error = error;
-            }
-            if (session_trace) {
-                await this.client.post(
-                  `https://api.honeyhive.ai/session/${this.session_id}/traces`,
-                  {
-                    logs: [session_trace],
-                  },
-                );
-
-                await this.client.put(
-                  `https://api.honeyhive.ai/events`,
-                  {
-                    event_id: this.session_id,
-                    outputs: outputs,
-                  }
-                );
-            }
-        } catch {
-            // continue regardless of error
-        }
-    }
+  }
 }
--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/*.test.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/*.test.ts"],
   testTimeout: 60000,
 };
-
